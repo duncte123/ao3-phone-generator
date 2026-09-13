6be51931bd6ac925bc6a321ef9b1fedd72529125
duncte123 <contact@duncte123.me>
2026-09-13 20:39:09 +0200

    look ma, I moddled a type

--- src/types/PhoneContent.ts
+++ src/types/PhoneContent.ts
@@ -1,3 +1,27 @@
 export interface PhoneContent {
-  //
+  recipient: string;
+  isGroup: boolean;
+  chatName: string;
+  messages: Array<Message>;
 }
+
+export type Message = TextMessage | TimeMessage | ImageMessage;
+
+export interface TimeMessage extends Message {
+  type: 'time';
+  day: string;
+  time: string;
+}
+
+export interface ImageMessage {
+  type: 'img';
+  url: string;
+  desc: string;
+}
+
+export interface TextMessage {
+  type: 'message';
+  name: string;
+  text: string;
+}
+
